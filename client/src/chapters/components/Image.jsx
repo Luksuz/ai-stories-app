6d7651179799334d4b3e18fa05f3b6bc4af42d2d
Jure Perak <jure.perak@moberg.hr>
2023-12-01 09:13:49 +0100

    Netlify rules

--- client/src/chapters/components/Image.jsx
+++ client/src/chapters/components/Image.jsx
@@ -2,11 +2,7 @@ export const Image = ({ image }) => {
     return (
         <>
             {image ? (
-                <img
-                    src={image}
-                    className="chapter__article--image"
-                    alt="Image that describes the chapter"
-                />
+                <img src={image} className="chapter__article--image" alt="Describes the chapter" />
             ) : (
                 <div role="status" className="chapter__article--loader">
                     <div className="chapter__article--loader-item"></div>
